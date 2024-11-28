4218a39ed78ac838b939d8399fc1523adeadb8b7
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2024-11-28 11:36:52 -0600

    add: astro pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import AstroPWA from '@vite-pwa/astro'
+
 import react from '@astrojs/react';
 
 import icon from 'astro-icon';
 
 import mdx from '@astrojs/mdx';
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://swumplurd.github.io",
   output: 'static',
-  integrations: [tailwind(), react(), icon(), mdx()]
+  integrations: [tailwind(), react(), icon(), mdx(), AstroPWA()]
 });
